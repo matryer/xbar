84011415de6bffbb0b398bfa982ddf2286731b88
Mat Ryer <mat@stretchr.com>
2013-11-13 15:31:57 -0700

    added preferences app

---
+++ BitBarPreferences/BitBarPreferences/BitBarPreferences.h
@@ -0,0 +1,15 @@
+//
+//  BitBarPreferences.h
+//  BitBarPreferences
+//
+//  Created by Mat Ryer on 11/13/13.
+//  Copyright (c) 2013 Bit Bar. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface BitBarPreferences : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ BitBarPreferences/BitBarPreferences/BitBarPreferences.m
@@ -0,0 +1,17 @@
+//
+//  BitBarPreferences.m
+//  BitBarPreferences
+//
+//  Created by Mat Ryer on 11/13/13.
+//  Copyright (c) 2013 Bit Bar. All rights reserved.
+//
+
+#import "BitBarPreferences.h"
+
+@implementation BitBarPreferences
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
