9d8dd1a9a07fb78dc4e36735befa708906d0e487
Mat Ryer <mat@stretchr.com>
2013-11-25 16:42:37 -0700

    added another tutorial

---
+++ Plugins/Tutorial/cycle_text_and_detail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Cycle text and detail text
+# BitBar plugin
+#
+# by Mat Ryer
+#
+# Text above --- will be cycled through in the menu bar,
+# whereas text underneath will be visible only when you
+# open the menu.
+
+echo one
+echo two
+echo three
+echo ---
+echo These lines are only visible
+echo when you open the menu.
