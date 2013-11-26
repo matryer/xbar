9c923489149fac84d94fff44f1a15f9160f20cb7
Mat Ryer <mat@stretchr.com>
2013-11-25 18:08:39 -0700

    added other coinbase options

--- Plugins/BitCoins/coinbase.com/buy.10s.sh
+++ Plugins/BitCoins/coinbase.com/buy.10s.sh
@@ -5,8 +5,7 @@
 #
 # by Mat Ryer
 #
-# Shows latest Buy and Sell values (in USD) for Bitcoins in the
+# Shows latest buy values (in USD) for Bitcoins in the
 # Coinbase exchange.
 
 echo -n "Buy: "; curl -s "https://coinbase.com/api/v1/prices/buy?currency=USD" | egrep -o ',"amount":"[0-9]+(\.)?([0-9]{0,2}")?' | sed 's/,"amount"://'  | sed 's:^.\(.*\).$:\1:'
-echo -n "Sell: "; curl -s "https://coinbase.com/api/v1/prices/sell?currency=USD" | egrep -o ',"amount":"[0-9]+(\.)?([0-9]{0,2}")?' | sed 's/,"amount"://'  | sed 's:^.\(.*\).$:\1:'
---
+++ Plugins/BitCoins/coinbase.com/sell.10s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Coinbase.com Buy and Sell
+# BitBar plugin
+#
+# by Mat Ryer
+#
+# Shows latest sell values (in USD) for Bitcoins in the
+# Coinbase exchange.
+
+echo -n "Sell: "; curl -s "https://coinbase.com/api/v1/prices/sell?currency=USD" | egrep -o ',"amount":"[0-9]+(\.)?([0-9]{0,2}")?' | sed 's/,"amount"://'  | sed 's:^.\(.*\).$:\1:'
