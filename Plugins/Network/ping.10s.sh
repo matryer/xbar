7caee7f69ff99c4b66c7271552e8c7e945b86432
Grant Sherrick <agsherrick@gmail.com>
2016-01-05 14:33:49 -0700

    Update shell script to use array and change to use loops.

--- Plugins/Network/ping.10s.sh
+++ Plugins/Network/ping.10s.sh
@@ -5,80 +5,71 @@
 # It shows current ping to some servers at the top Menubar
 # This helps me to know my current connection speed
 #
-# Author: trungdq88@gmail.com
+# Authors: (Trung Đinh Quang) trungdq88@gmail.com and (Grant Sherrick) https://github.com/thealmightygrant 
 
 MAX_PING=10000
-ping_google=$(ping -c 2 google.com | awk -F '/' 'END {printf "%d\n", $5}')
-ping_facebook=$(ping -c 2 facebook.com | awk -F '/' 'END {printf "%d\n", $5}')
-ping_github=$(ping -c 2 github.com | awk -F '/' 'END {printf "%d\n", $5}')
-ping_stackoverflow=$(ping -c 2 stackoverflow.com | awk -F '/' 'END {printf "%d\n", $5}')
-ping_dota2=$(ping -c 2 sgp-1.valve.net | awk -F '/' 'END {printf "%d\n", $5}')
+SITES=(google.com youtube.com wikipedia.org github.com imgur.com)
 
-# If ping == 0, that means the ping request has problem
-if (( $ping_google == 0 )) ; then
-    ping_google=$MAX_PING
-fi
+#grab ping times for all sites
+SITE_INDEX=0
+PING_TIMES=
 
-if (( $ping_facebook == 0 )) ; then
-    ping_facebook=$MAX_PING
-fi
-
-if (( $ping_github == 0 )) ; then
-    ping_github=$MAX_PING
-fi
-
-if (( $ping_stackoverflow == 0 )) ; then
-    ping_stackoverflow=$MAX_PING
-fi
-
-if (( $ping_dota2 == 0 )) ; then
-    ping_dota2=$MAX_PING
-fi
+while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
+    NEXT_SITE="${SITES[$SITE_INDEX]}"
+    NEXT_PING_TIME=$(ping -c 2 -n -q "$NEXT_SITE" 2>/dev/null | awk -F '/' 'END {printf "%d\n", $5}')
+    if [ "$NEXT_PING_TIME" -eq 0 ]; then
+        NEXT_PING_TIME=$MAX_PING
+    fi
+    if [ -z "$PING_TIMES" ]; then
+        PING_TIMES=($NEXT_PING_TIME)
+    else
+        PING_TIMES=(${PING_TIMES[@]} $NEXT_PING_TIME)
+    fi
+    SITE_INDEX=$(( $SITE_INDEX + 1 ))
+done
 
 # Calculate the average ping
-avg=$(( ($ping_google + $ping_facebook + $ping_github + $ping_stackoverflow + $ping_dota2)/5 ))
-# Standard deviation
-d1=$(( ($ping_google - avg)**2 ))
-d2=$(( ($ping_facebook - avg)**2 ))
-d3=$(( ($ping_github - avg)**2 ))
-d4=$(( ($ping_stackoverflow - avg)**2 ))
-d5=$(( ($ping_dota2 - avg)**2 ))
-sum=$(( ($d1 + $d2 + $d3 + $d4 + $d5)/5 ))
-sd=$(echo "sqrt ( $sum )" | bc -l | awk '{printf "%d\n", $1}')
+SITE_INDEX=0
+AVG=0
+while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
+    AVG=$(( ($AVG + ${PING_TIMES[$SITE_INDEX]}) ))
+    SITE_INDEX=$(( $SITE_INDEX + 1 ))
+done
+AVG=$(( $AVG / ${#SITES[@]} ))
+
+# Calculate STD dev
+SITE_INDEX=0
+AVG_DEVS=0
+while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
+    AVG_DEVS=$(( $AVG_DEVS + (${PING_TIMES[$SITE_INDEX]} - $AVG)**2 ))
+    SITE_INDEX=$(( $SITE_INDEX + 1 ))
+done
+AVG_DEVS=$(( $AVG_DEVS / ${#SITES[@]} ))
+SD=$(echo "sqrt ( $AVG_DEVS )" | bc -l | awk '{printf "%d\n", $1}')
 
 # Define color
-color="#cc3b3b"
-msg="$avg±$sd ⚡︎"
+COLOR="#cc3b3b"
+MSG="$AVG"'±'"$SD"'⚡'
 
-if (( $avg < 1000 )) ; then
-    color="#cc673b"
+if [ $AVG -ge 1000 ]; then
+    COLOR="#acacac"
+    MSG=" ☠ "
+elif [ $AVG -ge 600 ] && [ $AVG -lt 1000 ]; then
+    COLOR="#cc673b"
+elif [ $AVG -ge 300 ] && [ $AVG -lt 600 ]; then
+    COLOR="#ce8458"
+elif [ $AVG -ge 100 ] && [ $AVG -lt 300 ]; then
+    COLOR="#6bbb15"
+elif [ $AVG -ge 50 ] && [ $AVG -lt 100 ]; then
+    COLOR="#0ed812"
+else
+    COLOR="#e506ff"
 fi
 
-if (( $avg < 600 )) ; then
-    color="#ce8458"
-fi
-
-if (( $avg < 300 )) ; then
-    color="#6bbb15"
-fi
-
-if (( $avg < 100 )) ; then
-    color="#0ed812"
-fi
-
-if (( $avg < 50 )) ; then
-    color="#e506ff"
-fi
-
-if (( $avg == $MAX_PING )) ; then
-    color="#acacac"
-    msg="☠️"
-fi
-
-echo "$msg | font=UbuntuMono-Bold color=$color size=10"
+echo "$MSG | font=UbuntuMono-Bold color=$COLOR size=10"
 echo "---"
-echo "Google: $ping_google ms"
-echo "Facebook: $ping_facebook ms"
-echo "GitHub: $ping_github ms"
-echo "Stack Overflow: $ping_stackoverflow ms"
-echo "DotA 2: $ping_dota2 ms"
+SITE_INDEX=0
+while [ $SITE_INDEX -lt ${#SITES[@]} ]; do
+    echo "${SITES[$SITE_INDEX]}: ${PING_TIMES[$SITE_INDEX]} ms"
+    SITE_INDEX=$(( $SITE_INDEX + 1 ))
+done
