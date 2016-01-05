52174835adf3c7a1487e3b01287f922bb2dfaee8
Dan Seripap <daniel@seripap.com>
2016-01-05 11:20:05 -0500

    Actual URL to get API key

--- Plugins/Weather/ForecastIO/weather.15m.py
+++ Plugins/Weather/ForecastIO/weather.15m.py
@@ -12,7 +12,7 @@ import urllib2
 from random import randint
 
 location = '40.7027,-73.9867' # Lat,long only. Find at forecast.io
-api_key = '' # get yours at api.forecast.io // ac2ca0d3783bb686c40c9144fd647a05
+api_key = '' # get yours at https://developer.forecast.io
 units = '' # change to si for metric
 
 def get_wx():
