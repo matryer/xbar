651cc1651411e2e736cf29f460a413c4b2df94f1
Đinh Quang Trung <trungdq88@gmail.com>
2016-01-05 20:56:48 +0700

    Standard deviation

--- Plugins/Network/ping.10s.sh
+++ Plugins/Network/ping.10s.sh
@@ -7,41 +7,48 @@
 #
 # Author: trungdq88@gmail.com
 
-exec 2>/dev/null
+MAX_PING=10000
+ping_google=$(ping -c 2 google.com | awk -F '/' 'END {printf "%d\n", $5}')
+ping_facebook=$(ping -c 2 facebook.com | awk -F '/' 'END {printf "%d\n", $5}')
+ping_github=$(ping -c 2 github.com | awk -F '/' 'END {printf "%d\n", $5}')
+ping_stackoverflow=$(ping -c 2 stackoverflow.com | awk -F '/' 'END {printf "%d\n", $5}')
+ping_dota2=$(ping -c 2 sgp-1.valve.net | awk -F '/' 'END {printf "%d\n", $5}')
 
-if ping_result=`ping -c 1 google.com`; then
-    ping_google=$(echo $ping_result | awk -F '/' 'END {printf "%d\n", $5}')
-else
-    ping_google=-1
+# If ping == 0, that means the ping request has problem
+if (( $ping_google == 0 )) ; then
+    ping_google=$MAX_PING
 fi
 
-if ping_result=`ping -c 1 facebook.com`; then
-    ping_facebook=$(echo $ping_result | awk -F '/' 'END {printf "%d\n", $5}')
-else
-    ping_facebook=-1
+if (( $ping_facebook == 0 )) ; then
+    ping_facebook=$MAX_PING
 fi
 
-if ping_result=`ping -c 1 github.com`; then
-    ping_github=$(echo $ping_result  | awk -F '/' 'END {printf "%d\n", $5}')
-else
-    ping_github=-1
+if (( $ping_github == 0 )) ; then
+    ping_github=$MAX_PING
 fi
 
-if ping_result=`ping -c 1 stackoverflow.com`; then
-    ping_stackoverflow=$(echo $ping_result  | awk -F '/' 'END {printf "%d\n", $5}')
-else
-    ping_stackoverflow=-1
+if (( $ping_stackoverflow == 0 )) ; then
+    ping_stackoverflow=$MAX_PING
 fi
 
-if ping_result=`ping -c 1 sgp-1.valve.net`; then
-    ping_dota2=$(echo $ping_result  | awk -F '/' 'END {printf "%d\n", $5}')
-else
-    ping_dota2=-1
+if (( $ping_dota2 == 0 )) ; then
+    ping_dota2=$MAX_PING
 fi
 
+# Calculate the average ping
 avg=$(( ($ping_google + $ping_facebook + $ping_github + $ping_stackoverflow + $ping_dota2)/5 ))
+# Standard deviation
+d1=$(( ($ping_google - avg)**2 ))
+d2=$(( ($ping_facebook - avg)**2 ))
+d3=$(( ($ping_github - avg)**2 ))
+d4=$(( ($ping_stackoverflow - avg)**2 ))
+d5=$(( ($ping_dota2 - avg)**2 ))
+sum=$(( ($d1 + $d2 + $d3 + $d4 + $d5)/5 ))
+sd=$(echo "sqrt ( $sum )" | bc -l | awk '{printf "%d\n", $1}')
 
+# Define color
 color="#cc3b3b"
+msg="$avg±$sd ⚡︎"
 
 if (( $avg < 1000 )) ; then
     color="#cc673b"
@@ -52,26 +59,23 @@ if (( $avg < 600 )) ; then
 fi
 
 if (( $avg < 300 )) ; then
-    color="#bbbc55"
+    color="#6bbb15"
 fi
 
 if (( $avg < 100 )) ; then
-    color="#59b86d"
+    color="#0ed812"
 fi
 
 if (( $avg < 50 )) ; then
     color="#e506ff"
 fi
 
-if (( $avg == 0 )) ; then
+if (( $avg == $MAX_PING )) ; then
     color="#acacac"
+    msg="☠️"
 fi
 
-if (( $avg < 0 )); then
-    color="#ff0000"
-fi
-
-echo "$avg ms | color=$color size=12"
+echo "$msg | font=UbuntuMono-Bold color=$color size=10"
 echo "---"
 echo "Google: $ping_google ms"
 echo "Facebook: $ping_facebook ms"
