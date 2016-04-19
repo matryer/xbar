bc2c55a75edc74ba38128a33b3603794733c6134
Ganesh Varadarajan <ganesh@coriolis.co.in>
2016-04-19 08:23:06 +0530

    Support for multi-line titles   echo "CPU\n10% | size=7" will be rendered on the menubar (or dropdown) as a single block of two lines.

--- App/BitBar/Plugin.m
+++ App/BitBar/Plugin.m
@@ -81,7 +81,8 @@
     [item setTarget:self];
   }
   BOOL parseANSI = [fullTitle containsANSICodes] && ![[params[@"ansi"] lowercaseString] isEqualToString:@"false"];
-  if (params[@"font"] || params[@"size"] || params[@"color"] || parseANSI)
+  BOOL multiline = [fullTitle rangeOfString:@"\\n"].location != NSNotFound;
+  if (params[@"font"] || params[@"size"] || params[@"color"] || parseANSI || multiline)
     item.attributedTitle = [self attributedTitleWithParams:params];
   
   if (params[@"alternate"]) {
@@ -100,6 +101,7 @@
 - (NSAttributedString*) attributedTitleWithParams:(NSDictionary *)params {
 
   NSString * fullTitle = params[@"title"];
+  fullTitle = [fullTitle stringByReplacingOccurrencesOfString:@"\\n" withString:@"\n"];
   if (![[params[@"emojize"] lowercaseString] isEqualToString:@"false"]) {
     fullTitle = [fullTitle emojizedString];
   }
