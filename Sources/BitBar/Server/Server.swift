1111eeb6088c8051bbbb58cc9a314cadd356b896
Linus Oleander <linus@oleander.nu>
2017-06-21 01:08:00 +0200

    Add comments to HTTP requests

--- Sources/BitBar/Server/Server.swift
+++ Sources/BitBar/Server/Server.swift
@@ -20,40 +20,48 @@ func startServer() throws -> Droplet {
   // log.addDestination(ConsoleDestination())
 
   drop.group("plugins") { group in
+    // PATCH /plugins/refresh
     group.patch("refresh") { _ in
       manager.refresh()
       return try ok("Plugin(s) has beeen refreshed")
     }
 
+    // GET /plugins
     group.get { _ in
       return try JSON(node: manager.pluginsNames)
     }
   }
 
+  // WS /log
   drop.socket("log") { _, ws in
     ws.setup()
   }
 
   drop.group("plugin", PluginFile.parameter) { plugin in
+    // GET /plugin/:plugin
     plugin.get { req in
       return try req.parameters.next(PluginFile.self).makeJSON()
     }
 
+    // PATCH /plugin/:plugin/hide
     plugin.patch("hide") { req in
       try req.parameters.next(PluginFile.self).hide()
       return try ok("Plugin is now hidden")
     }
 
+    // PATCH /plugin/:plugin/show
     plugin.patch("show") { req in
       try req.parameters.next(PluginFile.self).show()
       return try ok("Plugin is now visible")
     }
 
+    // PATCH /plugin/:plugin/refresh
     plugin.patch("refresh") { req in
       try req.parameters.next(PluginFile.self).refresh()
       return try ok("Plugin is now refreshed")
     }
 
+    // PATCH /plugin/:plugin/invoke/arg1/arg2
     plugin.patch("invoke", "*") { req in
       let plugin = try req.parameters.next(PluginFile.self)
       let args = req.uri.path.split("invoke/").last!.split("/")
