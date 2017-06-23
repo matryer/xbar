45e4198ed5766893eda7d028b2f3632aec2424c4
Linus Oleander <linus@oleander.nu>
2017-06-23 11:56:55 +0200

    Do not use binary data for websocket connection

--- Sources/BitBar/Server/SocketLog.swift
+++ Sources/BitBar/Server/SocketLog.swift
@@ -22,9 +22,18 @@ public class SocketLog: BaseDestination {
       return "[Fatal error] Could not create log message \(error)"
     }
 
-    let output = String(describing: json)
-    if (try? websocket.send(output)) == nil {
-      log.error("Could not send data to connected socket server")
+
+    guard let bytes = try? json.serialize(prettyPrint: true) else {
+      return "[Fatal error] Could not serialize json"
+    }
+
+    let data = NSData(bytes: bytes, length: bytes.count) as Data
+    guard let output = String(data: data, encoding: .utf8) else {
+      return nil
+    }
+
+    guard (try? websocket.send(output)) != nil else {
+      return "[Fatal error] Could not send data to connected socket server"
     }
 
     return output
