16dd3498c0a39efa60a22712e9ecc6aae78798f4
Mat Ryer <matryer@users.noreply.github.com>
2021-03-10 13:25:01 +0000

    slightly fatter buttons

--- app/frontend/src/elements/Button.svelte
+++ app/frontend/src/elements/Button.svelte
@@ -15,8 +15,8 @@
 
 	// classes are the css classes to use for each style of button
 	const classes = {
-		'default': 'text-sm rounded shadow-sm dark:shadow-none px-4 border bg-gray-100 active:bg-gray-300 dark:text-gray-300 dark:bg-gray-900 border-gray-300 dark:border-gray-600 dark:bg-opacity-50 hover:bg-opacity-75',
-		'primary': 'bg-blue-900 active:bg-blue-800 text-sm text-white rounded shadow-sm dark:shadow-none px-4 border border-blue-900 dark:bg-black dark:border-black dark:bg-opacity-50 hover:bg-opacity-75',
+		'default': 'text-sm rounded shadow-sm dark:shadow-none px-4 py-1 border bg-gray-100 active:bg-gray-300 dark:text-gray-300 dark:bg-gray-900 border-gray-300 dark:border-gray-600 dark:bg-opacity-50 hover:bg-opacity-75',
+		'primary': 'bg-blue-900 active:bg-blue-800 text-sm text-white rounded shadow-sm dark:shadow-none px-4 py-1 border border-blue-900 dark:bg-black dark:border-black dark:bg-opacity-50 hover:bg-opacity-75',
 	}
 
 	// css is the final css string to inject into the element.
