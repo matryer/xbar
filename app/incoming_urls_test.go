a799a020c346bd6018871deae3655d05f04acbea
matryer <thatmatryer@gmail.com>
2021-03-10 14:58:54 +0000

    realistic tests

--- app/incoming_urls_test.go
+++ app/incoming_urls_test.go
@@ -14,12 +14,12 @@ func TestParseIncomingURL(t *testing.T) {
 	is.Equal(result.Action, "openPlugin")
 	is.Equal(result.Params.Get("path"), "IoT/homebridge.10s.py")
 
-	result, err = parseIncomingURL(`xbar://app.xbarapp.com/refreshPlugin?name=cycle_text_and_detail`)
+	result, err = parseIncomingURL(`xbar://app.xbarapp.com/refreshPlugin?path=cycle_text_and_detail`)
 	is.NoErr(err)
 	is.Equal(result.Action, "refreshPlugin")
-	is.Equal(result.Params.Get("name"), "cycle_text_and_detail")
+	is.Equal(result.Params.Get("path"), "cycle_text_and_detail")
 
-	result, err = parseIncomingURL(`xbar://app.xbarapp.com/nope?name=cycle_text_and_detail`)
+	result, err = parseIncomingURL(`xbar://app.xbarapp.com/nope?path=cycle_text_and_detail`)
 	is.True(err != nil)
 
 }
