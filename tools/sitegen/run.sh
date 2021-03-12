49fd4884acd337a889fcae7e362750eb1656680e
matryer <thatmatryer@gmail.com>
2021-03-12 16:31:01 +0000

    added tests to critical flow

--- tools/sitegen/run.sh
+++ tools/sitegen/run.sh
@@ -7,3 +7,9 @@ echo -n "${VERSION}" > .version
 go build -o sitegen 
 ./sitegen
 rm sitegen
+
+# run the tests in xbarapp.com - we may have
+# just broken them
+cd ../../xbarapp.com
+echo -n "${VERSION}" > .version
+go test
