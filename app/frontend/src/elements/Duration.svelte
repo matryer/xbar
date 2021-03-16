556052321907abf8d73e1afb16452f8afb75d2ae
Mat Ryer <matryer@users.noreply.github.com>
2021-03-16 12:44:19 +0000

    show the right unit if ms

--- app/frontend/src/elements/Duration.svelte
+++ app/frontend/src/elements/Duration.svelte
@@ -24,7 +24,7 @@
 
 <style>
     .val {
-        width: 35px;
+        width: 45px;
         text-align: right;
     }
 </style>
@@ -33,20 +33,24 @@
     class='val border bg-gray-100 active:bg-gray-300 dark:text-gray-400 dark:bg-gray-700 border-gray-300 dark:border-gray-600 dark:bg-opacity-50' 
     type='number' 
     bind:value={value.n}
-    on:change={ onChange }
+    on:change={onChange}
     disabled={disabled}
 >
-<select 
-    class='mr-1 bg-gray-100 active:bg-gray-300 dark:text-gray-400 dark:bg-black border-gray-300 dark:border-gray-600'
-    bind:value={value.unit}
-    on:change={ onChange }
-    disabled={disabled}
->
-    <option>seconds</option>
-    <option>minutes</option>
-    <option>hours</option>
-    <option>days</option>
-</select>
+{#if value.unit == 'milliseconds'}
+    ms
+{:else}
+    <select 
+        class='mr-1 bg-gray-100 active:bg-gray-300 dark:text-gray-400 dark:bg-black border-gray-300 dark:border-gray-600'
+        bind:value={value.unit}
+        on:change={onChange}
+        disabled={disabled}
+    >
+        <option>seconds</option>
+        <option>minutes</option>
+        <option>hours</option>
+        <option>days</option>
+    </select>
+{/if}
 <Spinner 
     visible={spinnerVisible} 
     width='16' height='16'
