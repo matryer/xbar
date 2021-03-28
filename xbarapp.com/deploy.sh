58d355dca2d065e7a9151f94d094aadfaa1d6f2e
matryer <thatmatryer@gmail.com>
2021-03-28 10:04:49 +0100

    removed build step in deploy

--- xbarapp.com/deploy.sh
+++ xbarapp.com/deploy.sh
@@ -7,5 +7,4 @@ printf "package main\n\nconst version = \"${VERSION}\"" > version.gen.go
 echo "Deploying xbarapp.com (${VERSION})..."
 
 go test
-npm run build
 gcloud app deploy --project xbarapp --version=beta
