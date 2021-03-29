5544151e6777249648b5695e49c2590a04cdb7d1
matryer <thatmatryer@gmail.com>
2021-03-29 10:56:07 +0100

    load default variables from the plugin metadata #669

--- pkg/plugins/testdata/vars-test/plugin.sh
+++ pkg/plugins/testdata/vars-test/plugin.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# <xbar.var>string(XBAR_TEST_EXPLICIT_VAR): An explicit variable</xbar.var>
+# <xbar.var>string(XBAR_TEST_DEFAULT_VAR="default-value"): A default variable (from metadata)</xbar.var>
+# <xbar.var>string(XBAR_TEST_SET_IN_VARS_JSON): A variable set in the JSON</xbar.var>
+
 echo "XBAR_TEST_EXPLICIT_VAR=${XBAR_TEST_EXPLICIT_VAR}"
 echo "XBAR_TEST_SET_IN_VARS_JSON=${XBAR_TEST_SET_IN_VARS_JSON}"
+echo "XBAR_TEST_DEFAULT_VAR=${XBAR_TEST_DEFAULT_VAR}"
