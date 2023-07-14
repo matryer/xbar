06577f43eef85d7fe1c56a896782995ad1c4771a
Mat Ryer <matryer@users.noreply.github.com>
2023-07-14 18:49:34 +0100

    check case in test

--- pkg/metadata/categories_metadata_test.go
+++ pkg/metadata/categories_metadata_test.go
@@ -12,12 +12,12 @@ func TestCategoryMapper(t *testing.T) {
 	categories := make(map[string]Category)
 	CategoryEnsurePath(categories, nil, []string{"parent1", "child1", "grandchild1"})
 	CategoryEnsurePath(categories, nil, []string{"parent1", "child2", "grandchild1"})
-	CategoryEnsurePath(categories, nil, []string{"parent2", "child1", "grandchild1"})
+	CategoryEnsurePath(categories, nil, []string{"parent2", "Child1", "Grandchild1"})
 	CategoryEnsurePath(categories, nil, []string{"parent3", "child1"})
 
 	is.Equal(len(categories), 3)                                                            // parents
 	is.Equal(len(categories["parent1"].ChildrenCategories), 2)                              // parent1 children
-	is.Equal(len(categories["parent2"].ChildrenCategories["child1"].ChildrenCategories), 1) // parent2 child1 grandchildren
+	is.Equal(len(categories["parent2"].ChildrenCategories["Child1"].ChildrenCategories), 1) // parent2 child1 grandchildren
 
 	// b, err := json.MarshalIndent(segments, "", "\t")
 	// is.NoErr(err)
